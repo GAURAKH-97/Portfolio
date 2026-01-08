26e27353591d737d91a6fa1faad5758c674d2adc
Gaurakh <68327125+GAURAKH-97@users.noreply.github.com>
2026-01-08 17:00:42 +0530

    Update script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -84,7 +84,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing","web development"],
+    strings: ["a Frontend Developer", "a Backend Developer", "a Web Designer", "a Web Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
